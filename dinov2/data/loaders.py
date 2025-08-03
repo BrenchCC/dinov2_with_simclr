c7b464224185eee384cd69f7599f811ee6ad9594
BrenchCC <brenchchen.77@gmail.com>
2025-08-03 15:25:49 +0800

    add custom dataset for training custom dinov2

--- dinov2/data/loaders.py
+++ dinov2/data/loaders.py
@@ -10,7 +10,7 @@ from typing import Any, Callable, List, Optional, TypeVar
 import torch
 from torch.utils.data import Sampler
 
-from .datasets import ImageNet, ImageNet22k
+from .datasets import ImageNet, ImageNet22k, BrenchDataResourceDataset
 from .samplers import EpochSampler, InfiniteSampler, ShardedInfiniteSampler
 
 
@@ -58,6 +58,10 @@ def _parse_dataset_str(dataset_str: str):
             kwargs["split"] = ImageNet.Split[kwargs["split"]]
     elif name == "ImageNet22k":
         class_ = ImageNet22k
+    elif name == "BrenchDataResourceDataset":
+        class_ = BrenchDataResourceDataset
+        if "split" in kwargs:
+            kwargs["split"] = BrenchDataResourceDataset.Split[kwargs["split"]]
     else:
         raise ValueError(f'Unsupported dataset "{name}"')
 
